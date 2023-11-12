d94867fa2ba7913935ba511a9c5380d820afcc00
Dominic Cummings <dominiccummings95@gmail.com>
2023-11-12 09:17:23 -0600

    Moving onto 1.6-1.14

--- part1/part1/src/App.jsx
+++ part1/part1/src/App.jsx
@@ -1,18 +1,49 @@
 import { useState } from 'react'
 
-const App = () => {
-
-  const [ counter, setCounter ] = useState(0)
-
-
-  setTimeout(
-    () => setCounter(counter + 2),
-    1000
-  )
-  console.log('rendering...', counter)
+const History = (props) => {
+  if (props.allClicks.length === 0) {  
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
   return (
-    <div>{counter}</div>
+    <div>
+      button press history: {props.allClicks.join(' ')}
+    </div>
   )
 }
 
+const Button = ({ handleClick, text }) => {
+  console.log('props value: ', handleClick)
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const Display = props => <div>{props.value}</div>
+
+const App = () => {
+  const [value, setValue] = useState(10)
+
+  const setValueMe = (newValue) => () => {
+    setValue(newValue)
+    console.log('value now', newValue)
+  }
+  
+
+  return (
+    <div>
+      <Display value={value} />
+      <Button handleClick={setValueMe(1000)} text='thousand' />
+      <Button handleClick={setValueMe(0)} text='reset' />
+      <Button handleClick={setValueMe(value + 1)} text='increment' />
+    </div>
+  )
+}
 export default App
+
+
