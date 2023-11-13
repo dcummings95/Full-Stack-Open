728161b318d067b937a1a2f877037971b81eab59
Dominic Cummings <dominiccummings95@gmail.com>
2023-11-12 18:35:31 -0600

    1.12 anecdotes step1 done

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -24,7 +24,7 @@ const App = () => {
     setTotal(total - 1)
     setCount(count + 1)
   }
-  
+
   const average = ((total / count) * 100).toFixed(1) + '%'
   const posPercent = ((positive / count) * 100).toFixed(1) + '%'
   const totalFeedback = good + neutral + bad; 
